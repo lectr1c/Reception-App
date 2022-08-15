78959edd8cdb298f118c24161b9af3522a372ebd
lectr1c <66423346+lectr1c@users.noreply.github.com>
2022-08-15 10:13:56 +0200

    Removed Hero Text

--- components/Main/Hero.tsx
+++ components/Main/Hero.tsx
@@ -14,24 +14,17 @@ const Hero = () => {
             .then(r => {
                 console.log(r);
                 setTeams.setState([...r.data]);
-                const index =teams.findIndex((teamX) => {
-                    teamX.points === Math.max(...teams.map(team => team.points ? team.points : 0))
-                })
-
-                setWinningTeam(index)
             })
             .catch(err => {
                 console.log(err);
             })
     }, [])
 
-    const [winningTeam, setWinningTeam] = useState(0)
-
     return (
         <div className={styles.heroContainer}>
-            <div className={styles.heroMessageCon}>
-                <p>Laget <b>{teams.at(winningTeam)?.name}</b> leder med <b>{teams.at(winningTeam)?.points}</b> poäng!</p>
-            </div>
+            {/*<div className={styles.heroMessageCon}>*/}
+            {/*    <p>Laget <b>{winningTeam?.name}</b> leder med <b>{winningTeam?.points}</b> poäng!</p>*/}
+            {/*</div>*/}
             <div className={styles.graphContainer}>
                 <BarGraph teams={teams}/>
             </div>
