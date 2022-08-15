871685a99c353f7085a047e276ccfc225ce9eda4
lectr1c <66423346+lectr1c@users.noreply.github.com>
2022-08-15 03:59:58 +0200

    Update team.ts

--- pages/api/team.ts
+++ pages/api/team.ts
@@ -51,7 +51,8 @@ export default async function handler(
         await getToken({req})
             .then(r => {
                 if (!r) {
-                    res.status(403).json({name: "yoo"})
+                    // @ts-ignore
+                    res.status(403).json({name: "", ...r})
                     return;
                 }
       })}
